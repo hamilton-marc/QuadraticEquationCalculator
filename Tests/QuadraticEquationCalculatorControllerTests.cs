5284efe7b1895959f2d22b81a6681781f4ce9ee3
Marc Hamilton <unionhills@cox.net>
2023-06-10 12:52:28 -0700

    #3 feat: add unit test for controller

---
+++ Tests/QuadraticEquationCalculatorControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using QuadraticEquationCalculator.Server.Controllers;
+using Xunit.Abstractions;
+
+namespace QuadraticEquationCalculator.Tests
+{
+    public class QuadraticEquationCalculatorControllerTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public QuadraticEquationCalculatorControllerTests(ITestOutputHelper output)
+        {
+            this._outputHelper = output;
+        }
+
+        [Fact]
+        public void Constructor_ClassExists_ReturnsInstance()
+        {
+            var mockLogger = Mock.Of<ILogger<QuadraticEquationCalculatorController>>();
+            var controller = new QuadraticEquationCalculatorController(mockLogger);
+        }
+    }
+}
