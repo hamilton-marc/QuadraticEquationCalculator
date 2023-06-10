0f19f07139ead8e96b0f193fd0f5b866baebc158
Marc Hamilton <unionhills@cox.net>
2023-06-10 14:36:38 -0700

    #3 create object representations for the Complex roots

---
+++ Shared/QuadraticRoots.cs
@@ -0,0 +1,14 @@
+﻿namespace QuadraticEquationCalculator.Shared
+{
+    public class QuadraticRoot
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+    }
+
+    public class QuadraticRoots
+    {
+        public required QuadraticRoot Root1 { get; set; }
+        public required QuadraticRoot Root2 { get; set; }
+    }
+}
